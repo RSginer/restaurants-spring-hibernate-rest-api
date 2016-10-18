7328e17525021a335905bef13bc91efec078b70c
Ruben Soler <r.solerginer@gmail.com>
2016-10-18 20:33:17 +0200

    Añado codigos de respuesta HTTP OK, NO_CONTENT, BAD_REQUEST, INTERNAL_SERVER_ERROR

--- src/java/com/rsginer/spring/controllers/RestaurantesController.java
+++ src/java/com/rsginer/spring/controllers/RestaurantesController.java
@@ -9,11 +9,8 @@ import com.rsginer.exceptions.BussinessException;
 import com.rsginer.json.JsonTransformer;
 import com.rsginer.spring.dao.RestaurantesDAO;
 import com.rsginer.spring.model.Restaurante;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,60 +33,67 @@ public class RestaurantesController {
     @Autowired
     private RestaurantesDAO restaurantesDAO;
 
-    @RequestMapping(value = {"/restaurantes"}, method=RequestMethod.GET,
-                            produces = "application/json")
-    public void getRestaurantes(HttpServletRequest httpRequest, 
-                                HttpServletResponse httpServletResponse){
+    @RequestMapping(value = {"/restaurantes"}, method = RequestMethod.GET,
+            produces = "application/json")
+    public void getRestaurantes(HttpServletRequest httpRequest,
+            HttpServletResponse httpServletResponse) {
         try {
             List<Restaurante> listaRestaurantes = new ArrayList<>();
             listaRestaurantes = restaurantesDAO.findAll();
             String jsonSalida = jsonTransformer.toJson(listaRestaurantes);
+            if (listaRestaurantes.size() <= 0) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            }
             httpServletResponse.getWriter().println(jsonSalida);
+
         } catch (BussinessException ex) {
-            
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } catch (Exception ex) {
-           
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
-    
-    @RequestMapping(value = {"/restaurantes/{id}"}, method=RequestMethod.GET,
-                            produces = "application/json")
+
+    @RequestMapping(value = {"/restaurantes/{id}"}, method = RequestMethod.GET,
+            produces = "application/json")
     public void getRestauranteById(HttpServletRequest httpRequest,
-                                   HttpServletResponse httpServletResponse,
-                                   @PathVariable("id") int idRestaurante){
+            HttpServletResponse httpServletResponse,
+            @PathVariable("id") int idRestaurante) {
         try {
             Restaurante restaurante = restaurantesDAO.get(idRestaurante);
             String jsonSalida = jsonTransformer.toJson(restaurante);
+            if (restaurante == null) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_NO_CONTENT);
+            } else {
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+            }
             httpServletResponse.getWriter().println(jsonSalida);
+
         } catch (BussinessException ex) {
-          
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } catch (Exception ex) {
-           
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
-    
-    @RequestMapping(value = {"/restaurantes"}, method=RequestMethod.POST, 
-                            consumes = "application/json",
-                            produces = "application/json")
+
+    @RequestMapping(value = {"/restaurantes"}, method = RequestMethod.POST,
+            consumes = "application/json",
+            produces = "application/json")
     public void setRestaurante(HttpServletRequest httpRequest,
-                               HttpServletResponse httpServletResponse,
-                               @RequestBody String jsonEntrada){
+            HttpServletResponse httpServletResponse,
+            @RequestBody String jsonEntrada) {
         try {
             Restaurante restaurante = jsonTransformer.fromJSON(jsonEntrada, Restaurante.class);
             String jsonSalida = jsonTransformer.toJson(restaurante);
             restaurantesDAO.insert(jsonTransformer.fromJSON(jsonSalida, Restaurante.class));
             httpServletResponse.getWriter().println(jsonSalida);
+            httpServletResponse.setStatus(HttpServletResponse.SC_OK);
         } catch (BussinessException ex) {
-           
+            httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         } catch (Exception ex) {
-           
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
-    
-    
-    
-    
- 
+
 }
-
-
