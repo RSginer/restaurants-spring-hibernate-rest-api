e5fe23717b068e0090344ff584bf82e5954c8dc9
Ruben Soler <r.solerginer@gmail.com>
2016-10-18 19:16:26 +0200

    BD script SQL

---
+++ sql/BD.sql
@@ -0,0 +1,20 @@
+﻿CREATE DATABASE IF NOT EXISTS `webapp`;
+USE `webapp`;
+
+
+CREATE TABLE IF NOT EXISTS `restaurantes` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) DEFAULT NULL,
+  `direccion` text,
+  `descripcion` text,
+  `imagen` varchar(255) DEFAULT NULL,
+  `precio` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+DELETE FROM `restaurantes`;
+INSERT INTO `restaurantes` (`id`, `nombre`, `direccion`, `descripcion`, `imagen`, `precio`) VALUES
+	(1, 'Macdonals', 'Calle de la piruleta nº 2', 'Este es un restaurante que hacen hamburguesas', 'macdonals.jpg', '20000'),
+	(3, 'Bar manolo', 'Avenida de los naranjos', 'Bar familiar donde cenar con los niños', 'barmanolo.jpg', '50.000');
+
+
