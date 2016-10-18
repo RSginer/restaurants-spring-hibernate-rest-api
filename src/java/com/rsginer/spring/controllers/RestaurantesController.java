04ee17686998fcaa358852dcfe41bb0e8c569237
Ruben Soler <r.solerginer@gmail.com>
2016-10-18 19:31:53 +0200

    REST API metodo getRestauranteById OK

--- src/java/com/rsginer/spring/controllers/RestaurantesController.java
+++ src/java/com/rsginer/spring/controllers/RestaurantesController.java
@@ -5,15 +5,20 @@
  */
 package com.rsginer.spring.controllers;
 
+import com.rsginer.exceptions.BussinessException;
 import com.rsginer.json.JsonTransformer;
 import com.rsginer.spring.dao.RestaurantesDAO;
 import com.rsginer.spring.model.Restaurante;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -32,12 +37,23 @@ public class RestaurantesController {
 
     @RequestMapping(value = {"/restaurantes"}, method=RequestMethod.GET,
                             produces = "application/json; charset=UTF-8")
-    public void get(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws Exception {
+    public void getRestaurantes(HttpServletRequest httpRequest, HttpServletResponse httpServletResponse) throws Exception {
         List<Restaurante> listaRestaurantes = new ArrayList<>();
         listaRestaurantes = restaurantesDAO.findAll();
         String jsonListaRestaurantes = jsonTransformer.toJson(listaRestaurantes);
         httpServletResponse.getWriter().println(jsonListaRestaurantes);
     }
+    
+    @RequestMapping(value = {"/restaurantes/{id}"}, method=RequestMethod.GET,
+                            produces = "application/json; charset=UTF-8")
+    public void getRestauranteById(HttpServletRequest httpRequest,
+                                   HttpServletResponse httpServletResponse,
+                @PathVariable("id") int idRestaurante) throws BussinessException, IOException{
+            Restaurante restaurante = restaurantesDAO.get(idRestaurante);
+            String jsonRestaurante = jsonTransformer.toJson(restaurante);
+            httpServletResponse.getWriter().println(jsonRestaurante);
+    }
+    
  
 }
 
