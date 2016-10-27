62f004c8d0dd85687f39acb68a21a8f368ae8142
Rubén Soler <r.solerginer@gmail.com>
2016-10-27 10:17:39 +0200

    arreglo problema de carpeta vacia

--- sql/BD.sql
+++ sql/BD.sql
@@ -1,8 +1,4 @@
-﻿CREATE DATABASE IF NOT EXISTS `webapp`;
-USE `webapp`;
-
-
-CREATE TABLE IF NOT EXISTS `restaurantes` (
+CREATE TABLErestaurantes `restaurantes` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) DEFAULT NULL,
   `direccion` text,
@@ -12,7 +8,6 @@ CREATE TABLE IF NOT EXISTS `restaurantes` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-DELETE FROM `restaurantes`;
 INSERT INTO `restaurantes` (`id`, `nombre`, `direccion`, `descripcion`, `imagen`, `precio`) VALUES
 	(1, 'Macdonals', 'Calle de la piruleta nº 2', 'Este es un restaurante que hacen hamburguesas', 'macdonals.jpg', '20000'),
 	(3, 'Bar manolo', 'Avenida de los naranjos', 'Bar familiar donde cenar con los niños', 'barmanolo.jpg', '50.000');
